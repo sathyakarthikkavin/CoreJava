fe5afb132586a7486d6c8a17f76289cc7e1a0163
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-19 01:56:45 +0530

    operators-arithmetic

---
+++ Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+    //operators-arithmetic
+    public static void main(String[] args) {
+        int num=7;
+        num++;
+        System.out.println(num);
+        ++num;
+        System.out.println(num);//difference on when fetching the value
+
+
+    }
+}
