8cca03f931fbe4d53213003d38413852a8ebc882
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-19 02:58:24 +0530

    Relational operator-To check condition

---
+++ RelationalOperator.java
@@ -0,0 +1,10 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        boolean z= x>=y;//<,>,<=,>=,!=
+        System.out.println(z);
+
+    }
+
+}
