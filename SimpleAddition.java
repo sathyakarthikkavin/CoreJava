c4ee4d9f1c51e7b8e80e934f28d8090a6f9bc963
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-18 05:31:53 +0530

    Simple Addition Program

---
+++ SimpleAddition.java
@@ -0,0 +1,14 @@
+
+public class SimpleAddition {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int res;
+        res=num1+num2;
+        System.out.println(res);
+
+    }
+
+
+
+}
