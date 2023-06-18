7f59db3c21a942de872cb54df889b6144aab4b3e
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-19 03:08:28 +0530

    LogicalOperator-To check more than one condition,(&&,||,!)

---
+++ LogicalOperator.java
@@ -0,0 +1,10 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        int x=10;
+        int y=5,a=9,b=9;
+        boolean result=(x<y && a<b);//To check more than one condition,(&&,||,!)
+        System.out.println(result);
+
+    }
+
+}
