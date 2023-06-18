dd350698d16c93022bf4878de760e1af67469ab1
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-19 01:47:54 +0530

    Type promotion

--- TypePromotion.java
+++ TypePromotion.java
@@ -2,7 +2,7 @@ public class TypePromotion {
     public static void main(String[] args) {
         byte a=10;
         byte b=8;
-        int result=a*b;//type promotion
+        int result=a*b;//type promotion .
         System.out.println(result);
     }
 }
