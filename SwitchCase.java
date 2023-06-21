90d4cb87274c577a7dde03bb408600f46b48d3d8
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-21 17:56:03 +0530

    Switch Case: based on variable  value deciding the selective executable  code.

---
+++ SwitchCase.java
@@ -0,0 +1,21 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int n = 1;
+        switch (n) {
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tues");
+            case 3:
+                System.out.println("wed");
+            case 4:
+                System.out.println("thurs");
+                Default:
+                System.out.println("sunday");
+                break;
+
+        }
+
+    }
+}
