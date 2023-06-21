07554f76aa9eb58152497b0ec1f077575ad596e2
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-21 23:38:26 +0530

    Do while -it is used for run a block of code the least only once and checking the condition.

---
+++ DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("hi" + i);
+            i++;
+        } while (i <= 0);
+
+
+    }
+}
