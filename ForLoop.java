b5174149dd934921ab16e40aee484bcaff2a98a7
Sathya Karthik <sathyakarthik88@gmail.com>
2023-06-22 00:11:20 +0530

    for loop-Print three different statement in one line and run a block of code repeatedly.  #print

---
+++ ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.printf("#\t");
+
+            }
+            System.out.println("\n");
+
+        }
+    }
+}
