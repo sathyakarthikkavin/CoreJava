7f4556b303ac4f79aefbc230e232db5b82fb9f33
sathyakarthikkavin <135815302+sathyakarthikkavin@users.noreply.github.com>
2023-07-03 11:12:15 +0530

    Update FirstExProgram.java

--- FirstExProgram.java
+++ FirstExProgram.java
@@ -1,6 +1,7 @@
+
 public class FirstExProgram {
     //first class
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Jvm looking for this sentence
         System.out.println("Print the statement");
     }
 }
